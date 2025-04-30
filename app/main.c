2fd5f00692f7108be154b97ff29d09784537e13c
Ben <sps.tech.way@gmail.com>
2025-05-01 00:33:07 +0300

    just some sketch

---
+++ app/main.c
@@ -0,0 +1,11 @@
+//
+// Created by :: SPS :: on 30.04.2025.
+//
+#include <stdio.h>
+#include "pico/stdlib.h"
+
+int main() {
+    stdio_init_all();
+    printf("Jst an example!\n");
+    return 0;
+}
