8c03fb29cd9da686367d0b494244f56ecfc3b27f
Petr Suslov <sps.tech.way@gmail.com>
2025-05-28 14:24:21 +0300

    bring here some useful cmake checkers

--- template.cmake
+++ template.cmake
@@ -1,17 +1,23 @@
-# Import the Pico SDK
-include($ENV{PICO_SDK_PATH}/external/pico_sdk_import.cmake)
-
-# Let CMake pick up whatever generator you’re already using
-find_package(Python3 REQUIRED COMPONENTS Interpreter)
-
-# Make presets.json from .def
-execute_process(
-        COMMAND ${Python3_EXECUTABLE}
-        ${CMAKE_CURRENT_LIST_DIR}/generate_presets.py
-        --generator "${CMAKE_GENERATOR}"
-        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+# Check [pico-sdk]
+if (NOT DEFINED ENV{PICO_SDK_PATH})
+    message(FATAL_ERROR "Environment variable PICO_SDK_PATH is not set")
+endif()
+get_filename_component(_PICO_SDK_DIR
+        "${CMAKE_BINARY_DIR}/$ENV{PICO_SDK_PATH}"
+        REALPATH
 )
 
-# Pull in any helper functions you’ve put under cmake/
-list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/cmake")
-include(PicoHelpers)
+# Include the SDK import
+include("${_PICO_SDK_DIR}/external/pico_sdk_import.cmake")
+
+# Check [picotool]
+get_filename_component(_PICOTOOL_DIR
+        "${CMAKE_SOURCE_DIR}/$ENV{PICOTOOL_FETCH_FROM_GIT_PATH}"
+        REALPATH
+)
+set(PICOTOOL_FETCH_FROM_GIT_PATH "${_PICOTOOL_DIR}" CACHE PATH "" FORCE)
+
+# Warn if not found
+if (NOT EXISTS "${PICOTOOL_FETCH_FROM_GIT_PATH}")
+    message(WARNING "Your shared picotool path does not exist: ${PICOTOOL_FETCH_FROM_GIT_PATH}")
+endif()
