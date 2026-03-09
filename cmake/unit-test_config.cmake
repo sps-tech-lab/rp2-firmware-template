765bd10f1b089066d43c1f9d98a062db78ab3ce2
sps <sps.tech.way@gmail.com>
2026-03-09 23:18:26 +0200

    refactor unit test configuration a bit | make it clearer

---
+++ cmake/unit-test_config.cmake
@@ -0,0 +1,22 @@
+function(config_unit_tests)
+
+    message(STATUS "Configure unit-tests")
+
+    project(rp2-firmware-template LANGUAGES C CXX)
+
+    # Enable clang-tools like targets
+    include(cmake/clang_tools.cmake)
+    rp2_enable_clang_tools()
+
+    # Shared libraries
+    add_subdirectory(src/demo)
+
+    enable_testing()
+    add_subdirectory(tests)
+
+endfunction()
+
+
+
+
+
